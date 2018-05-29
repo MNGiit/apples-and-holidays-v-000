f1b8c84713896a919e82c2132498d7c2f7c5c851
Mohammed Noori <kmanoorib@gmail.com>
2018-05-29 20:08:29 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -68,7 +68,7 @@ def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each do |season, data|
     puts "#{season.capitalize}:"
     data.each do |holiday, supply|
-      puts "  #{holiday.capitalize}: "
+      puts "  #{holiday.capitalize}: #{supply.join}"
       
     end
   end
